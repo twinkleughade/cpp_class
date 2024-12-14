c07c0fca46df1a729bd47635748cc4ef6cd10f8b
twinkleughade <twinklethakre1998@gmail.com>
2024-12-14 14:22:48 +0530

    add

---
+++ class_example.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+class joy
+{
+   int a=10 ;
+   public:void show()
+   {
+    cout<<a;
+   }
+
+};
+int main()
+{
+    joy d;
+    d.show();
+}
+
---
+++ syntax.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+// class classname
+// {
+//   datatype variable;
+//   public:datatype functionname(........)
+//   {
+//     statement/code;
+//   };
+//   int main()
+//   {
+//     classname objectname;
+//     objectname.functionname();
+//   }
+// }
